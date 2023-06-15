228e4ad235c812e1e0256b2f96f6c69c93ea6d9f
ipapMaster <wmast@inbox.ru>
2023-06-15 19:59:07 +0300

    Loop Using Start

--- main.py
+++ main.py
@@ -3,20 +3,13 @@ import turtle as t
 dist = 200
 sides = 6
 angle = 360 / sides
+counter = 0
 
 t.shape('turtle')  # задаём форму черепахи
 
-t.forward(dist)  # magic value
-t.right(angle)
-t.forward(dist)
-t.right(angle)
-t.forward(dist)
-t.right(angle)
-t.forward(dist)
-t.right(angle)
-t.forward(dist)
-t.right(angle)
-t.forward(dist)
-t.right(angle)
+while counter < 6:
+    t.forward(dist)
+    t.right(angle)
+    counter = counter + 1
 
 t.mainloop()  # ожидание действий пользователя
