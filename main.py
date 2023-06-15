5a9f9a8f9ce2c9e3c7cbc17238be5b04c9473de2
ipapMaster <wmast@inbox.ru>
2023-06-15 21:11:44 +0300

    Drawing N of M-angle

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import turtle as t
 
-dist = 30
-sides = 30
-angle = 360 / sides
-counter = 0
+dist = 60
+figs = 12
+FS = 8  # число сторон фигуры
+f_angle = 360 / FS  # угол фигуры
+angle = 360 / figs
+f_counter = 0  # счётчик фигур
 
 t.shape('turtle')  # задаём форму черепахи
 
-while counter < sides:
-    t.forward(dist)
+while f_counter < figs:
+    s_counter = 0
+    while s_counter < FS:
+        t.forward(dist)
+        t.left(f_angle)
+        s_counter += 1
     t.right(angle)
-    counter = counter + 1
+    f_counter += 1
 
 t.mainloop()  # ожидание действий пользователя
