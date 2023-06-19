c1147e15557376556ec96ca659a00e10e1018073
ipapMaster <wmast@inbox.ru>
2023-06-19 21:19:27 +0300

    Spiral

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-okroshka = ['колбаса', 'картофель', 'огурцы', 'яйца',
-            'зелень', 'кефир', 'сметана', 'вода', 'соль']
+import turtle as t
 
-for i in range(101):
-    if i % 10 == 7:
-        print(i)
+colors = ['red', 'purple', 'blue', 'green',
+          'yellow', 'orange', 'cyan', 'magenta', 'pink']
+t.bgcolor('black')
+t.speed(0)  # мгновенная скорость
+angle = 39  # нестандартный поворот :)
+for x in range(200):
+    t.pencolor(colors[x % 9])
+    t.width(x // 100 + 1)
+    t.forward(x)
+    t.left(angle)
+
+t.mainloop()
