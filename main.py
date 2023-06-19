fb0d0db624ef0af7b4885c256cfb8afe9077f241
ipapMaster <wmast@inbox.ru>
2023-06-19 19:59:49 +0300

    List Print using join()

--- main.py
+++ main.py
@@ -4,10 +4,5 @@ python_group += ['Терентьев']
 python_group += ['Петров', 'Сидоров']
 python_group += ['Творогов']
 
-print('В группе', len(python_group), 'чел.\nВот они:')
-
-counter = 0
-
-while counter < len(python_group):
-    print('\t', str(counter + 1) + '.', python_group[counter])
-    counter += 1
+print('В группе', len(python_group), 'чел.\nВот они', end=': ')
+print(', '.join(python_group) + '.')
