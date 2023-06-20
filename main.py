aca410b4e609dc38a058c3ecd8e4d3e8970f902a
ipapMaster <wmast@inbox.ru>
2023-06-20 20:58:29 +0300

    Example of Simple Branching

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-# списочное выражение
-# классический способ
-a = []
-for i in range(10):
-    a.append(i)
-print(*a)  # результат классического способа
+steps_yest = 15850  # шаги за вчера
+steps_today = 3950  # шаги за сегодня
+str_out = 'Вы прошли '
+steps_sum = steps_yest + steps_today
 
-# способ с использованием списочного выражения
-a = [i for i in range(10)]  # список от нуля до 9
+if steps_sum >= 10000:
+    print('Поздравляю!!!', end=' ')
+    gyges = steps_sum // 10000
+    str_out += str(steps_sum)
+    str_out += ' шагов. Вам стало доступно ' + str(gyges) + 'Гб. '
+    str_out += '\nДо следующего "гига" Вам осталось пройти '
+    str_out += str(10000 * gyges - (steps_sum - 10000))
+    str_out += ' шаг(ов).'
+else:
+    str_out += 'пока что ' + str(steps_sum) + ' шагов):'
+    str_out += '\nДо получения 1 Гб Вам следует пройти ещё '
+    str_out += str(10000 - steps_sum) + ' шаг(ов).'
 
-print(*a)   # выводим список, полученный
-            # списочным выражением
+print(str_out)
