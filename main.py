c9a9081bde1332787ae055a64567f9ded9291ba7
ipapMaster <wmast@inbox.ru>
2023-06-20 21:23:01 +0300

    Correct Greeting 1

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-steps_yest = 15850  # шаги за вчера
-steps_today = 3950  # шаги за сегодня
-str_out = 'Вы прошли '
-steps_sum = steps_yest + steps_today
+from datetime import datetime
 
-if steps_sum >= 10000:
-    print('Поздравляю!!!', end=' ')
-    gyges = steps_sum // 10000
-    str_out += str(steps_sum)
-    str_out += ' шагов. Вам стало доступно ' + str(gyges) + 'Гб. '
-    str_out += '\nДо следующего "гига" Вам осталось пройти '
-    str_out += str(10000 * gyges - (steps_sum - 10000))
-    str_out += ' шаг(ов).'
+get_time = datetime.time(datetime.now())
+cur_time = get_time.strftime("%H:%M:%S")
+h = int(get_time.strftime("%H"))
+print('На часах', cur_time)
+
+if h >= 6 and h < 12:
+    print('Доброе утро!')
+elif h >= 12 and h < 18:
+    print('Добрый день!')
+elif h >= 18 and h < 23:
+    print('Добрый вечер!')
 else:
-    str_out += 'пока что ' + str(steps_sum) + ' шагов):'
-    str_out += '\nДо получения 1 Гб Вам следует пройти ещё '
-    str_out += str(10000 - steps_sum) + ' шаг(ов).'
-
-print(str_out)
+    print('Доброй ночи!')
