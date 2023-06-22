8dea578a37d724461027ff2398c97557b8ca96e2
ipapMaster <wmast@inbox.ru>
2023-06-22 21:29:30 +0300

    Function Introducing

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# Словари - dictionary
-d = dict()  # пустой словарь
-string = 'Изучение языка Python'
-# удаление всех пробелов
-naked_string = ''.join(string.split())
+# Функции built-in
+""" help(int) - помощь по функции
+print(), input(), str(), float(), int(), round(),
+range(), reversed(), len(), list(), dict()
+"""
 
-# перебираем строку по буквам
-for ch in naked_string:
-    if ch in d:  # если ключ (буква) в словаре есть
-        d[ch] += 1  # увеличиваем значение на 1
-    else:  # если ключа (буквы) в словаре нет
-        d[ch] = 1  # то создаём ключ и задаём ему значение - 1
 
-print('Частотный анализ строки', '"' + string + '":')
-for k, v in d.items():
-    print(k, '-', v)
+def say_hello(name='bro'):
+    print('Hello,', name)
+
+
+say_hello()
+say_hello('Bill')
