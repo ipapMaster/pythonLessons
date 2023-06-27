a4a60ae5f8981f1d7def2053d827c778438811f4
ipapMaster <wmast@inbox.ru>
2023-06-27 19:16:39 +0300

    Four Methods with letters size

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-# res = ord(string[0])  # Код буквы P = 80 (код по символу)
-# res = chr(189)  # Символ с кодом 80 - P (символ по коду)
+string = 'pytHon lAnguage'
 
-# string = 'Python'  # UTF-8 (0 - 255)  -> UTF-16 (0 - 65535)
-string = 'S|wkrq'
-encrypted = []
-
-for letter in string:
-    code = ord(letter)
-    code -= 3
-    new_letter = chr(code)
-    encrypted.append(new_letter)
-
-print(*encrypted, sep='')
+print(string.upper())  # PYTHON LANGUAGE
+print(string.lower())  # python language
+print(string.capitalize())  # Python language
+print(string.title())  # Python Language
