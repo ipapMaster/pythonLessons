ba529d4b08b3e0b68acfd59f628a44e621f7763f
ipapMaster <wmast@inbox.ru>
2023-06-27 19:44:43 +0300

    Search in Loop

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 string = 'Видеть, вертеть, смотреть.'
-# find('что ищем', start, stop)
-index = string.find('еть')
-print(index)
 
-index = string.find('еть', 7)
-print(index)
+# получили число вхождений подстроки "еть"
+count = string.count('еть')
+print(count)  # и выведем его
 
-index = string.find('еть', 15, 20)
-print(index)
+counter = 0  # счётчик проходов по строке
+indexes = []  # тут будем хранить индексы вхождений
+index = 0  # откуда ищем (с какого индекса)
+
+while counter < count:
+    if counter == 0: # для первого прохода идём с нуля
+        index = string.find('еть', index)
+    else:
+        index = string.find('еть', index + 1)
+    indexes.append(index)
+    counter += 1
+
+print(*indexes)
