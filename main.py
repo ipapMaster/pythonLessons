8a7abd15cfc0b8c131e6009715e473c375a6208e
ipapMaster <wmast@inbox.ru>
2023-06-29 20:59:50 +0300

    Image Crop

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ from PIL import Image, ImageFilter
 
 original = Image.open('python.png')
 
-blur = original.filter(ImageFilter.BLUR)
-blur.save('blur.png')
-blur = original.filter(ImageFilter.BoxBlur(5))
-blur.save('boxblur.png')
-blur = original.filter(ImageFilter.GaussianBlur(5))
-blur.save('gaussblur.png')
+cropped = original.crop((60, 280, 370, 525))
 
-
+cropped.save('cropped.png')
