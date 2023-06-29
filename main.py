587088e6b9c174b29459f7e096ee2b803ccf258a
ipapMaster <wmast@inbox.ru>
2023-06-29 21:02:04 +0300

    Image Contour

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from PIL import Image, ImageFilter
 
 original = Image.open('python.png')
 
-cropped = original.crop((60, 280, 370, 525))
+contour = original.filter(ImageFilter.CONTOUR)
 
-cropped.save('cropped.png')
+contour.save('contour.png')
