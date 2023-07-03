f798866f61049fe7d7fb32df119b863aa653db7e
ipapMaster <wmast@inbox.ru>
2023-07-03 21:14:42 +0300

    File Write by print function

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ import os
 fname = 'info.txt'
 text = ''
 
-if os.path.isfile(fname):
-    f = open(fname, 'rt', encoding='utf-8')  # открыт на чтение (текстовый файл)
-    text = f.read(6)  # курсор файла остановится на 6-й позиции
-    f.read(5)
-    text += f.read(9)
-    f.close()
-else:
-    print('Файл не существует!')
+files = os.listdir()
 
-print(f'Из файла прочитано {len(text)} байт.')
-if text:
-    print(f'Вот этот текст: {text}')
+f = open(fname, 'wt')
+for file in files:
+    print(file, file=f)
+f.close()
+
+
+
