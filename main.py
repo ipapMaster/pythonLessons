227714cc6f7825cef0294882416deadfffbbd4a4
ipapMaster <wmast@inbox.ru>
2023-07-04 18:39:24 +0300

    Content Manager (with open)

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ fname = 'info.txt'
 text = ''
 
 if os.path.isfile(fname):
-    f = open(fname, 'rt', encoding='utf-8')
-    text = f.read()  # читаем строку целиком
-    f.close()
+    with open(fname, 'rt', encoding='utf-8') as f:
+        text = f.read()  # читаем строку целиком
+        # другие операции с открытым файлом
+
+# f.close() в этом случае уже не нужно
 
 if text:
     res = text.splitlines()  # строковый метод, разбивающий строку на элементы списка по \n
