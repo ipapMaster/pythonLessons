d9f44bd25a71e9b5ecdd72ee22a97fd4262d00eb
ipapMaster <wmast@inbox.ru>
2023-07-06 18:38:04 +0300

    How to get know exception name

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ n = input('Введите целое число: ')
 
 try:
     m = int(n)
-except ValueError:
+except Exception as exp:
+    print('Исключение:', exp.__class__.__name__)
     print(f'Вас просили ввести целое число, а Вы ввели "{n}".')
 else:
     print('Результат:', m / 2)
