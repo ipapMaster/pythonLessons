50fbdc7cab06bc6f7c6486e7c4cab53f87edb70c
ipapMaster <wmast@inbox.ru>
2023-07-06 20:40:09 +0300

    Regular Expressions Intro (Match)

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-while True:
-    a = input('Введите первое число: ')
-    b = input('Введите второе число: ')
-    try:
-        result = int(a) / int(b)  # попытка деления
-    except ValueError as exp:  # некорректный ввод
-        print('Одно из чисел введено некорректно!')
-        print(exp)
-    except ZeroDivisionError:  # деление на ноль
-        print('На ноль делить нельзя!')
-    else:
-        int_part = int(result)
-        fraction_part = result - int_part
-        if fraction_part == 0.0:
-            print(int(result))
-        else:
-            print(round(result, 2))  # первый способ
-            print(f'{result:.2f}')  # второй способ
-        break
+# Регулярные выражения (Regular Expressions)
+# Поиск по паттерну (pattern)
+import re
+
+pattern = '20'
+testString = '20 плюс 10 = 30'
+
+# метод match ищет совпадение только в начале строки
+result = re.match(pattern, testString)
+
+# span - индекс начали и конца найденной подстроки
+# match - собственно найденная подстрока
+
+print(result)
