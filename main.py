6c88b9fe264b17f30f67d6b725cc0459dcb9ee9a
ipapMaster <wmast@inbox.ru>
2023-07-10 21:27:53 +0300

    Constructor Introducing

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
 # ООП
-class Fruit:
-    pass
+
+class Car:
+    def __init__(self):
+        self.engine_on = False
+
+    def start_engine(self):
+        self.engine_on = True
+
+    def drive(self, where):
+        if self.engine_on:
+            print('Едем', where)
+        else:
+            print('Мотор не заведён.')
 
 
-class Greeter:
-    def hello(self):
-        print('Hello')
-
-    def hello_to_name(self, name):
-        print('Hello,', name)
-
-
-greet1 = Greeter()
-greet1.hello()  # Greeter.hello()
-
-greet2 = Greeter()
-greet2.hello()
-
-greet2.hello_to_name('Tom')
-
-apple = Fruit()
-apple.name = 'Яблоко'
-apple.weight = 150
-
-appricot = Fruit()
-appricot.name = 'Абрикос'
-appricot.weight = 50
-appricot.weight += 10
-
-print(appricot.weight)
+car = Car()
+car.start_engine()
+car.drive('на работу!')
