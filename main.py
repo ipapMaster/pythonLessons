b5fa4039d429759759a80138377bbe4b758dc732
ipapMaster <wmast@inbox.ru>
2023-07-10 21:17:24 +0300

    OOP Introducing

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-# Декораторы
-
-def time_elapsed(func):  # Декоратор
-    import time
-
-    def wrapper():
-        start = time.time()  # засекли время
-        func()
-        end = time.time()  # выключили секундомер
-        print(f'Потрачено времени: {end - start} сек.')
-    return wrapper
-
-@time_elapsed
-def long_list_creation():
-    # создаём список из 1'000'000 значений
-    a = [i for i in range(10000000)]
+# ООП
+class Fruit:
+    pass
 
 
-long_list_creation()
+class Greeter:
+    def hello(self):
+        print('Hello')
+
+    def hello_to_name(self, name):
+        print('Hello,', name)
+
+
+greet1 = Greeter()
+greet1.hello()  # Greeter.hello()
+
+greet2 = Greeter()
+greet2.hello()
+
+greet2.hello_to_name('Tom')
+
+apple = Fruit()
+apple.name = 'Яблоко'
+apple.weight = 150
+
+appricot = Fruit()
+appricot.name = 'Абрикос'
+appricot.weight = 50
+appricot.weight += 10
+
+print(appricot.weight)
