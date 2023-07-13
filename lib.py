864495482a83b70f0da1836942fdfc688b30f69e
ipapMaster <wmast@inbox.ru>
2023-07-13 21:08:32 +0300

    Magic Method __add__

--- lib.py
+++ lib.py
@@ -1,66 +1,15 @@
-from tkinter import *
-import requests
-from urllib.request import urlopen
-from io import BytesIO
-from PIL import Image, ImageTk
+class Time:
+    def __init__(self, minutes, seconds):
+        self.minutes = minutes
+        self.seconds = seconds
 
-class App:
-    def __init__(self):
-        self.win = Tk()  # создаём объект окна
-        self.win.title('Информация о погоде')
-        self.win.geometry('700x400')
-        self.win.resizable(False, False)
-        # Иконка на Ваш выбор - скачать в корень проекта файл icon.png
-        # self.win.iconphoto(True, PhotoImage(file='icon.png'))
-        self.title = Label(text='Погода', background='yellow',
-                           foreground='red', font=('Verdana', 18))  # Формат цвета '#45ff78'
-        self.title.pack(fill=X)
-        self.canvas = Canvas(bg='white', width=100, height=100)
-        self.canvas.pack(anchor=CENTER, pady=10)
-        mess = StringVar()
-        mess.set('Курск')
-        self.town = Entry(width=40, font=('Verdana', 15), textvariable=mess)
-        self.town.pack(side=LEFT, fill=X, anchor=N, padx=50)
-        self.btn = Button(text='Получить', command=self.get_weather)
-        self.btn.pack(side=LEFT, fill=X, anchor=N)
-        self.temper = Label(font=('Verdana', 18))
-        self.temper.place(x=50, y=210)
-        self.flike = Label(font=('Verdana', 18))
-        self.flike.place(x=50, y=250)
-        self.hmd = Label(font=('Verdana', 18))
-        self.hmd.place(x=50, y=290)
-        self.icon = None
-        # Ожидание действий пользователя
-        self.win.mainloop()
+    def __add__(self, other):
+        m = self.minutes + other.minutes
+        s = self.seconds + other.seconds
+        m += s // 60
+        s = s % 60
+        return Time(m, s)
 
-    def get_weather(self):
-        if self.town.get() == '':
-            town = 'Санкт Петербург'
-        else:
-            town = self.town.get()
-        # полученный ключ
-        key = 'c747bf84924be997ff13ac5034fa3f86'
-        # API - Server
-        url = 'http://api.openweathermap.org/data/2.5/weather'
-        # Дополнительные параметры
-        params = {'APPID': key, 'q': town, 'units': 'metric'}
-        # Отправляем запрос:
-        response = requests.get(url, params=params)
-        result = response.json()  # читаем отсюда
-        code = result['cod']  # прочли код ответа
-        if code != 200:
-            self.temper['text'] = 'Нет информации'
-            self.flike['text'] = ''
-            self.hmd['text'] = ''
-        else:
-            temper = result['main']['temp']  # температура
-            feels_like = result['main']['feels_like']  # ощущается как
-            humidity = result['main']['humidity']  # влажность
-            icon_temp = result['weather'][0]['icon']  # иконка
-            self.url = f'https://openweathermap.org/img/wn/{icon_temp}@2x.png'
-            icon = urlopen(self.url).read()
-            self.image = ImageTk.PhotoImage(Image.open(BytesIO(icon)))
-            self.canvas.create_image(0, 0, anchor=NW, image=self.image)
-            self.temper['text'] = f'Температура (город: {town}): {temper: .1f} \xB0C'
-            self.flike['text'] = f'Ощущается как: {feels_like: .1f} \xB0C'
-            self.hmd['text'] = f'Влажность: {humidity: .1f} %'
+    def time_info(self):
+        return f'{self.minutes}:{self.seconds}'
+
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-# ООП - Полиморфизм
-from lib import App
+# ООП - Спецметоды
+from lib import Time
 
 if __name__ == '__main__':
-    app = App()
+    t1 = Time(5, 50)
+    t2 = Time(3, 20)
+
+    t3 = t1 + t2
+
+    print(t3.time_info())
