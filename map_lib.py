747fcf58f31c5e164b5b0a1fd5bd5eaeb9ff820d
ipapMaster <wmast@inbox.ru>
2023-07-20 20:05:42 +0300

    Map Search Final Version

--- map_lib.py
+++ map_lib.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from io import BytesIO
 import requests
-from tkinter import NW, X
+from tkinter import NW, X, END
 from tkinter import Tk, Button, PhotoImage, Label, Canvas, Entry
 from PIL import ImageTk, Image
 
@@ -22,6 +22,10 @@ class App:
         self.header = Label(text='Адрес:', font=('Verdana', 12))
         self.entry = Entry(width=50, font=('Verdana', 14))
         self.button = Button(text='Найти', command=self.search)
+        self.plus = Button(text='+', font=('Verdana', 14),
+                           command=self.positive_zoom)
+        self.minus = Button(text=chr(8212), font=('Verdana', 14),
+                            command=self.negative_zoom)
         self.canvas = Canvas(width=600, height=450)
         # размещаем элементы
         self.header.place(x=15, y=35)
@@ -29,6 +33,9 @@ class App:
         self.button.place(x=700, y=35)
         self.canvas.place(x=100, y=75)
         self.label_clock.place(x=320, y=540)
+        self.plus.place(x=475, y=540)
+        self.minus.place(x=250, y=540)
+        self.delta = '0.001'  # для изменения spn
         # добавляем изображение-пустышку
         image = Image.new('RGB', (600, 450), (255, 255, 255))
         self.orig = image
@@ -37,9 +44,14 @@ class App:
         self.show_time()  # вызов метода отображения часов
 
     def search(self):
+        self.what_to_find = self.entry.get().strip()
+        if len(self.what_to_find) < 3:
+            self.entry.delete(0, END)
+            self.entry.insert(END, 'СПб, Можайская, 2')
+            self.what_to_find = self.entry.get()
         geocoder_params = {
             'apikey': '40d1649f-0493-4b70-98ba-98533de7710b',
-            'geocode': 'СПб, Можайская, 2',
+            'geocode': self.what_to_find,
             'format': 'json'
         }
         url = 'http://geocode-maps.yandex.ru/1.x/'
@@ -49,7 +61,7 @@ class App:
         coords = coords.split()
         map_params = {
             'll': ','.join(coords),
-            'spn': '0.005,0.005',
+            'spn': f'{self.delta},{self.delta}',
             'l': 'map',
             'pt': ','.join(coords) + ',pm2dgl'
         }
@@ -59,8 +71,19 @@ class App:
         self.photo = ImageTk.PhotoImage(self.orig)
         self.canvas.create_image(0, 0, anchor=NW, image=self.photo)
 
+    def positive_zoom(self):
+        temp = float(self.delta) / 2
+        if temp < 0.00025:
+            temp = 0.00025
+        self.delta = str(temp)
+        self.search()
 
-
+    def negative_zoom(self):
+        temp = float(self.delta) * 2
+        if temp > 32.768:
+            temp = 32.768
+        self.delta = str(temp)
+        self.search()
 
     def forever(self):
         self.root.mainloop()
