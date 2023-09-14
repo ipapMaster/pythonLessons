8ac73da6f9a535dbff63426cfae01527694da474
ipapMaster <wmast@inbox.ru>
2023-09-14 18:54:54 +0300

    TextArea rendering 2

--- addpost.py
+++ addpost.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField  # , TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 # ORM - Object Relational Model (объектно-реляционное отображение)
@@ -7,5 +7,5 @@ from wtforms.validators import DataRequired
 # pip install sqlalchemy
 class NewPost(FlaskForm):
     title = StringField('Заголовок', validators=[DataRequired('Заголовок обязателен')])
-    content = StringField('Контент', validators=[DataRequired('Введите контент')])
+    content = TextAreaField('Контент', validators=[DataRequired('Введите контент')])
     submit = SubmitField('Добавить')
