c42ff9e021888697251fcdca1418accc490dc4c6
ipapMaster <wmast@inbox.ru>
2023-09-28 20:16:13 +0300

    CLI Num Converter

--- cli_test.py
+++ cli_test.py
@@ -1,9 +1,48 @@
 import sys
 
-length = len(sys.argv)
 
-if length > 1:
-    print(f'Первый аргумент {sys.argv[1]}')
-    print(f'Последний аргумент {sys.argv[-1]}')
+def print_help(msg=''):
+    print(f'Используйте так: {sys.argv[0]} [-h] [--log LOG] [--base BASE] int [int ...]\n{msg}')
+
+
+def main(args):
+    integers = []
+    log_file = ''
+    base = 2
+
+    while args:
+        arg = args.pop(0)
+        if arg == '-h':
+            print_help()
+            return None, None
+        elif arg == '--base':
+            try:
+                base = int(args.pop(0))
+            except ValueError:
+                print_help(f'Некорректный параметр: {arg}')
+                return None, None
+        elif arg == '--log':
+            log_file = args.pop(0)
+        else:
+            integers.append(arg)
+
+    if not integers:
+        print('Ничего нет')
+        return None, None
+
+    try:
+        return list(map(lambda x: int(x, base), integers)), log_file
+    except ValueError as e:
+        print_help(f'Некорректное значение {e}')
+        return None, None
+
+
+numbers, log_file = main(sys.argv[1:])
+
+if log_file is None:
+    pass
+elif log_file == '':
+    print(*numbers)
 else:
-    print('Скрипт запущен без аргументов')
+    with open(log_file, 'wt') as output:
+        print(*numbers, file=output)
